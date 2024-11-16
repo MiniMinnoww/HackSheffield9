1c9c612f4c757d842ec9ec38621214b7e404db74
Matthew Salmon <miniminnowcodes@gmail.com>
2024-11-16 15:43:33 +0000

    Reorganised 23

--- backend/server.py
+++ backend/server.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("frontend/index.html")  # Serve the HTML page
+    return render_template("index.html")  # Serve the HTML page
 
 @app.route('/api/data', methods=['GET', 'POST'])
 def api_data():
     if request.method == 'POST':
-        data = request.json  # Parse JSON data from the frontend
+        data = request.json  # Parse JSON data from the templates
         print(data)  # Process the data (e.g., save to DB, perform calculations)
         return jsonify({"message": "Data received!"})
     return jsonify({"data": "Hello from the backend!"})
