d35a4b46fa3ece3abb93a313ffa0047f2b3aa1ba
Matthew Salmon <miniminnowcodes@gmail.com>
2024-11-16 18:21:50 +0000

    beep boop

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,5 +1,27 @@
 console.log("Started JS!")
 
+// Fetch data from Flask
+let fetch_data = () => {
+    fetch('/api/data') // Call Flask's GET endpoint
+        .then(response => response.json())
+        .then(data => {
+            console.log(JSON.stringify(data, null, 2))
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
+// Send data to Flask
+let send_data = (payload) => {
+    fetch('/api/data', {method: 'POST', headers: {'Content-Type': 'application/json',}, body: JSON.stringify(payload),})
+        .then(response => response.json())
+        .then(data => {
+            console.log(JSON.stringify(data, null, 2))
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
 const COLS = 32
 const ROWS = 24
 
@@ -43,9 +65,39 @@ class Cell {
     }
 }
 
+class ChordCell extends Cell {
+    updateUI() {
+        this.element.classList.remove(this.enabled ? "cell-off" : "chord-cell-on")
+        this.element.classList.add(this.enabled ? "chord-cell-on" : "cell-off")
+    }
+
+    toggle_on() {
+        this.enabled = true
+        this.updateUI()
+    }
+}
+
 midi_notes = []
+chords = []
 midi_roll = document.getElementById("midi_roll")
 
+let chordRow = document.createElement("div")
+chordRow.classList.add("row")
+midi_roll.appendChild(chordRow)
+
+// Create chord cells
+for (let n = 0; n < COLS; n++) {
+    let cell = document.createElement("div")
+    chordRow.appendChild(cell)
+    cell.classList.add("cell")
+
+    let chordCell = new ChordCell(n, -1, cell)
+    if (n % 8 === 0) chordCell.toggle_on()
+
+    chords.push(chordCell)
+}
+
+// Make MIDI roll cells
 for (let i = 0; i < ROWS; i++) {
     let row = []
 
@@ -65,31 +117,28 @@ for (let i = 0; i < ROWS; i++) {
 
 }
 
-// // Fetch data from Flask
-// fetch = () => {
-//     fetch('/api/get-data') // Call Flask's GET endpoint
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(JSON.stringify(data, null, 2))
-//         })
-//         .catch(error => console.error('Error:', error));
-// }
-//
-//
-// // Send data to Flask
-// send = () => {
-//     const payload = { name: "Alice", age: 30 }; // Example data to send
-//
-//     fetch('/api/post-data', { // Call Flask's POST endpoint
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(payload),
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(JSON.stringify(data, null, 2))
-//         })
-//         .catch(error => console.error('Error:', error));
-// };
+document.getElementById("goButton").addEventListener("click", (e) => {
+    let generatePayload = () => {
+        let payload = {}
+        let note = 0
+
+        payload["chords"] = ""
+        for (let chord of chords) {
+            payload["chords"] += chord.enabled ? "1" : "0"
+        }
+
+        for (let row of midi_notes) {
+            payload[note] = ""
+            for (let cell of row) {
+                payload[note] += cell.enabled ? "1" : "0"
+            }
+
+            note++
+        }
+
+        return payload
+    }
+
+    let payload = generatePayload()
+    send_data(payload)
+})
