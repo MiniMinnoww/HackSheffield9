14f90944bb75d281964737c8bd2d92501115af45
fraser <fraserwbrown@outlook.com>
2024-11-16 13:57:31 +0000

    nursery rhymes

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("test2")
 print("Emily was here")
 print("Jess has no idea what's happening")
 print("What the sigma?!")
+
+for b in range (10000, 0, -1):
+    print(b, "green bottles hanging on a wall")
+    print(b, "green bottles hanging on a wall")
+    print("and if 1 green bottle should accidentally fall")
+    print("there'd be", (b-1), "green bottles hanging on a wall")
