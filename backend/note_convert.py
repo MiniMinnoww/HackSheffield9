eba4017f63c8b985b234451631086121e121e0e8
fraser <fraserwbrown@outlook.com>
2024-11-16 18:29:23 +0000

    receive notes

--- backend/note_convert.py
+++ backend/note_convert.py
@@ -7,7 +7,7 @@ def on_data_received(payload):
 def split_note_profile(note_profile, chord_profile):
     splits = list()
     for pos in range(len(note_profile)):
-        if chord_profile[pos] == "1" or note_profile[pos] == "1":
+        if chord_profile[pos] == "1" or (note_profile[pos] == "1" and note_profile[pos - 1] == "0"):
             pass
 
 split_note_profile(example_note_profile, example_chord_profile)
