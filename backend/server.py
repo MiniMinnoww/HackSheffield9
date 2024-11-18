dcfed0b0666bc417b8847fc5e07f57d1258c46a4
Matthew Salmon <miniminnowcodes@gmail.com>
2024-11-18 11:54:36 +0000

    Woo key centre

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import note_convert
+from backend.logic import maj_template
+from backend.note_convert import example_payload, print_chord_dict
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,6 +13,8 @@ def index():
 def api_data():
     if request.method == 'POST':
         data = request.json  # Parse JSON data from the templates
+        key = note_convert.notes[get_key_centre(get_all_notes(data))]
+        print(f"{key[0]} maj is the key with {key[1]} probability")
         # print(data)
 
         # data_returned = [{"root": 0, "type": "maj", "length": 8}, {"root": 7, "type": "maj", "length": 8},
@@ -19,6 +24,34 @@ def api_data():
 
         return jsonify(data_returned)
     return jsonify({"data": "Hello from the backend!"})
+
+def get_all_notes(payload):
+    notes = {}
+    for note, note_string in payload.items():
+        if note.isdigit():
+            midi_note = int(note)
+            notes[midi_note] = note_string.count('1')
+    return notes
+
+def get_key_centre(melody_notes):
+    possibilities = {}
+    total = 0
+
+    # Go through each key
+    for i in range(12):
+        possibilities[i] = 0
+        for note_in_scale in maj_template.values():
+            for note in melody_notes:
+                if melody_notes[note] == 0: continue
+                note_mod = note % 12
+                if note_mod == (note_in_scale + i) % 12:
+                    possibilities[i] += melody_notes[note]
+                    total += melody_notes[note]
+    sorted_possibilities = dict(sorted(possibilities.items(), key=lambda item: item[1], reverse=True))
+    return list(sorted_possibilities.keys())[0], f"{(sorted_possibilities[list(sorted_possibilities.keys())[0]] / total)}%"
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
