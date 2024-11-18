9ff088b5d273163976a5d5193f4795b39ab19442
Matthew Salmon <miniminnowcodes@gmail.com>
2024-11-18 13:05:39 +0000

    more

--- backend/static/js/notes.js
+++ backend/static/js/notes.js
@@ -18,7 +18,7 @@ recalculateInterval()
 let playing = false
 
 // Function to play a tone with fade in and fade out
-function playTone(frequency, duration, amp=0.2) {
+function playTone(frequency, duration, amp=0.4) {
     const oscillator = audioContext.createOscillator()
 
     const DURATION_SECONDS = duration / 1000
@@ -160,7 +160,7 @@ let getChordNotes = (root, chordType) => {
 
 let playChord = (notes, duration) => {
     for (let note of notes) {
-        playTone(midi_note_to_freq(note + 12), duration, 0.1)
+        playTone(midi_note_to_freq(note + 12), duration, 0.2)
     }
 }
 
